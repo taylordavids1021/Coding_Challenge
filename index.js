288475e00c9a1937993feceffab85c73ae3ddc8b
Taylor <taylordavids997@gmail.com>
2023-11-06 10:05:56 +0200

    create an array that preview 11 numbers

--- index.js
+++ index.js
@@ -1,46 +1,5 @@
-const data = {
-	lists: [
-		['first', [11, 7, 5]],
-		['second', [2, 6, 8, 4, 10]],
-		['third', [9, 3, 1]],
-	]
-}
+// Create an array that holds numbers
+const result = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
 
-const [hide_First, first] = data.lists[0];
-const [hide_Second, second] = data.lists[1];
-const [hide_Third, third] = data.lists[2];
-
-
-const result = []
-
-const extractBiggest = () => {
-	let first_Value = first[first.length -1]
-	let second_Value = second[second.length -1]
-	let third_Value = third[third.length -1]
-
-	const highest = Math.max(first_Value || 0,second_Value || 0,third_Value || 0)
-	if (first_Value == highest) {
-		return first.pop()
-	} else if (second_Value == highest) {
-		return second.pop()
-	} else {
-		return third.pop()
-	}
-
-}
-
-result.push(extractBiggest())
-result.push(extractBiggest())
-result.push(extractBiggest())
-
-result.push(extractBiggest())
-result.push(extractBiggest())
-result.push(extractBiggest())
-
-result.push(extractBiggest())
-result.push(extractBiggest())
-result.push(extractBiggest())
-result.push(extractBiggest())
-result.push(extractBiggest())
-
+// Preview array in console log
 console.log(result)
